c328479663555e7d98d18aeda7bf83f7683b5bca
defi3 <84861633+defi3@users.noreply.github.com>
2021-05-31 21:37:53 -0400

    Add lend

---
+++ contracts/lend/IMarket.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+interface IMarket {
+    function getToken() external view returns (address);
+    function supplyOf(address account) external view returns (uint);
+    function borrowBy(address account) external view returns (uint);
+    function updatedSupplyOf(address account) external view returns (uint);
+    function updatedBorrowBy(address account) external view returns (uint);
+    function accrueInterest() external;
+    function transferTo(address sender, address receiver, uint amount) external;
+}
+
